2de8108def968edffe9676b190f35995c890a632
M4thez <namemat@gmail.com>
2025-05-07 18:16:15 +0200

    Hotfix #1 - encoded password

--- WebApp/backend/opensearchClient.js
+++ WebApp/backend/opensearchClient.js
@@ -1,8 +1,18 @@
 const { Client } = require('@opensearch-project/opensearch');
 require('dotenv').config();
 
+const user = process.env.OPENSEARCH_USER;
+const rawPassword = process.env.OPENSEARCH_PASSWORD;
+
+if (!user || !rawPassword) {
+    console.error("OpenSearch user or password not set in environment variables!");
+    process.exit(1);
+}
+
+const encodedPassword = encodeURIComponent(rawPassword);
+
 const client = new Client({
-    node: `${process.env.OPENSEARCH_SCHEME}://${process.env.OPENSEARCH_USER}:${process.env.OPENSEARCH_PASSWORD}@${process.env.OPENSEARCH_HOST}:${process.env.OPENSEARCH_PORT}`
+    node: `${process.env.OPENSEARCH_SCHEME}://${user}:${encodedPassword}@${process.env.OPENSEARCH_HOST}:${process.env.OPENSEARCH_PORT}`
 });
 
 client.ping()
